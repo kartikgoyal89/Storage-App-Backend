23ab26e989ecaac6c3143d70fa92cf2d80a1eba6
Kartik Goyal <kartikgoyal794@gmail.com>
2026-05-06 23:43:04 +0530

    changes

--- utils/sendOtp.js
+++ utils/sendOtp.js
@@ -4,16 +4,23 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.gmail.com",
+//   port: 587,
+//   secure: false, // use TLS
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
+
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // use TLS
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
-
 transporter.verify((err, success) => {
   if (err) {
     console.log(err);
